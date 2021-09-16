68f1892a20234e5d9bbf182060076323288b494f
Azer Masimov <azer.mesimov@starex.az>
2021-09-17 03:22:58 +0400

    added delete button

--- src/components/App.js
+++ src/components/App.js
@@ -1,50 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ToDo from './ToDo';
 import './App.css';
 
-class App extends React.Component {
-    state = {
-        willPushToList: "",     // push to list arr
-        list: [],
-    }
+const App = () => {
+    const [todo, setTodo] = useState("");
+    const [todos, setTodos] = useState([]);
 
-    handleInput = (e) => {
-        // console.log(this.state.list);
+    const handleInput = (e) => {
         e.preventDefault();
-        const list = [...this.state.list];
-        list.push(this.state.willPushToList);
-
-        console.log(list);
-
-        this.setState({
-            willPushToList: "",
-            list,
-        });
+        setTodos([...todos, todo]);
+        setTodo("");
     }
 
-    render() {
-        return(
-            <div className="ui container">
-                    <div className="ui form">
-                        <form className="ui input">
-                            <input
-                                type="text"
-                                className="" 
-                                placeholder="Type your task here!" 
-                                value={this.state.willPushToList} 
-                                onChange={(e) => this.setState({ willPushToList: e.target.value})} 
-                            />
-                            <button className="ui button" onClick={this.handleInput} >
-                                Add
-                            </button>
-                        </form>
+    const deleteItem = (index) => {
+        const deleted = [...todos];
+        setTodos(deleted.filter((item) => deleted.indexOf(item) !== index));
+    }
+
+    return(
+        <div className="ui form two column centered grid container">
+            <div className="two column centered row segment">
+                <form className="ui grid column">
+                    <input
+                        type="text"
+                        className="left floated fourteen wide column" 
+                        placeholder="Type your task here!" 
+                        value={todo} 
+                        onChange={(e) => setTodo(e.target.value)} 
+                    />
+                    <div className="right floated two wide column">
+                        <i className="plus square outline icon" onClick={handleInput} ></i>
                     </div>
-                    <div className="">
-                        <ToDo listItems={this.state.list} />
-                    </div>
+                </form>
             </div>
+            <div className="column">
+                <ToDo 
+                    listItems={todos} 
+                    deleteItem={deleteItem}
+                />
+            </div>
+        </div>
         );
     }
-}
 
 export default App;
--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -2,14 +2,23 @@ import './ToDo.css';
 
 const ToDo = (props) => {
     return (
-        <ul className="ui list">
+        <ul className="ui segment">
             {
-                props.listItems.map((item, index) => { 
-                    return( 
-                        <li key={index}>
-                            <div className="ui checkbox">
-                                <input type="checkbox" />
-                                <label>{item}</label>
+                props.listItems.map((item, index) => {
+                    const onClickCallBack = () => {
+                        props.deleteItem(index);
+                    }
+
+                    return(
+                        <li key={index} className="">
+                            <div className="ui grid">
+                                <div className="ui checkbox left floated five wide column">
+                                    <input type="checkbox" />
+                                    <label>{item}</label>
+                                </div>
+                                <div className="right floated five wide column" onClick={onClickCallBack} >
+                                    <i class="times circle outline icon"></i>
+                                </div>
                             </div>
                         </li>
                     );
