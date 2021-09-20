0a1836cde5044305ffc99a9cfe9b1217ad02198a
Azer Masimov <azer.mesimov@starex.az>
2021-09-20 13:31:01 +0400

    added Checkbox

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { Fragment, useState } from 'react';
 import ToDo from './ToDo';
 import './App.css';
 
@@ -27,6 +27,7 @@ const App = () => {
             if (checkedArr.indexOf(element) === index) {
                 element.isCompleted = item.isCompleted;
             }
+            return element;
         }));
     }
 
@@ -35,31 +36,34 @@ const App = () => {
     }
 
     return(
-        <div className="ui form two column centered grid container">
-            <div className="two column centered row segment">
-                <form className="ui form grid column" onSubmit={handleInput}>
-                    <input
-                        type="text"
-                        className="left floated fourteen wide column" 
-                        placeholder="Type your task here!" 
-                        value={todo} 
-                        onChange={(e) => setTodo(e.target.value)} 
+        <Fragment>
+            <div className="ui form two column centered grid container">
+                <h1 style={{margin: 40, fontSize: 40}}>Todos App</h1>
+                <div className="two column centered row segment">
+                    <form className="ui form grid column" onSubmit={handleInput}>
+                        <input
+                            type="text"
+                            className="left floated fourteen wide column" 
+                            placeholder="Type your task here!" 
+                            value={todo} 
+                            onChange={(e) => setTodo(e.target.value)} 
+                        />
+                        <div className="right floated two wide column">
+                            <i className="plus square outline icon" onClick={handleInput} ></i>
+                        </div>
+                    </form>
+                </div>
+                <div className="ui grid column">
+                    <ToDo 
+                        listItems={todos} 
+                        deleteItem={deleteItem}
+                        inputCheckbox={inputCheckbox}
                     />
-                    <div className="right floated two wide column">
-                        <i className="plus square outline icon" onClick={handleInput} ></i>
-                    </div>
-                </form>
+                    <div>You have <span className="num-span">{[...todos].length}</span> task(s) To Do</div>
+                    <button onClick={clearAll} className="ui button right floated">Clear All</button>
+                </div>
             </div>
-            <div className="ui grid column">
-                <ToDo 
-                    listItems={todos} 
-                    deleteItem={deleteItem}
-                    inputCheckbox={inputCheckbox}
-                />
-                <div>You have <span className="num-span">{[...todos].length}</span> task(s) To Do</div>
-                <button onClick={clearAll} className="ui button right floated">Clear All</button>
-            </div>
-        </div>
+        </Fragment>
         );
     }
 
--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -12,19 +12,22 @@ const ToDo = (props) => {
 
                     const onChangeCB = (e) => {
                         // console.log(e.target.checked);
-                        props.listItems.isCompleted = e.target.checked;
+                        item.isCompleted = e.target.checked;
                         props.inputCheckbox(item, index);
                     }
 
+                    let bgColor = item.isCompleted === true ? "black" : "";
+                    let checkedColor = item.isCompleted === true ? "white" : ""
+
                     return(
-                        <div key={index} className="">
+                        <div key={index} className=""  style={{backgroundColor: bgColor}}>
                             <div className="ui grid">
                                 <div className="left floated five wide column">
                                     <div className="ui checkbox">
                                         <input type="checkbox" onChange={onChangeCB} />
                                         <label>
-                                            {item.isCompleted === true ? <del>{item.todo}</del> : item.todo}
-                                            </label>
+                                            {item.isCompleted === true ? <del style={{color: checkedColor}}>{item.todo}</del> : item.todo}
+                                        </label>
                                     </div>
                                 </div>
                                 <div className="right floated five wide column" onClick={onClickCB} >
